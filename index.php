056bd83d76b7596a1ace3b45c43e6dbe1aad0e59
ramiro ochoa <ventas@dshop.com.co>
2014-03-02 01:26:53 -0500

    index.html to index.php
    index.php structure was finished dividing the content by header.php and
    footer.php witch are going to be re-used in other pages, structure
    finished missing for functionality.

--- index.php
+++ index.php
@@ -19,64 +19,28 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <?php include ('site/header.php') ?> <!--header-->
 
-          ga('create', 'UA-42264987-1', 'dshop.me');
-          ga('send', 'pageview');
-
-        </script>
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
-
-        <!--Header-->
-        <header>
-            <div class="logo">
-                <a href="http://dshop.me" alt=""><img src="http://i.imgur.com/8zHEJLa.png" alt="Dshop inventory software" /></a>
-            </div>
-            <div class="nav">
-              <ul>
-                <li class="long_pricing"><a href="#">See prices & start a free trial</a></li>
-                <li class="pricing"><a href="#">Pricing</a></li>
-                <li class="long_tour"><a href="#">Check out an example project</a></li>
-                <li class="tour"><a href="#">Tour</a></li>
-                <li class="contact"><a href="#">Contact</a></li>
-                <li class="blog"><a href="http://dshopme.blogspot.com/" target="_blank">Blog</a></li>
-              </ul>
-            </div>
-        </header>
-        <!--Header-->
-
-        <!--Section****************************************-->
         <section>
 
              <article class="step1">
                  <div class="main_picture">
-                  <img src="http://i.imgur.com/J2uGvdt.png" alt="dshop" />
+                  <img src="img/prueba6.png" alt="dshop" />
                  </div>
                  <div class="page_welcome">
-                  <h1>The smarter inventory software</h1>
-                  <p>Wanna know how Dshop can increase your store's sales and business growth?.</p>
+                  <h1>The #1 inventory software for stores</h1>
+                  <p>Dshop is here to increase your store's sales and accelerate business growth.</p>
                     <form action"#" method="POST" class="subscription_form">
                          <label>Hey! What's your name?</label>
                          <input type="text" name="full_name" maxlength="40" required />
                          <label>May i have your email?</label>
                          <input type="email" name="email_address"required /> 
-                         <input type="submit" value="Start free trial" />
+                         <input type="submit" class="call_action_blue" value="Get started 30-Day free trial" />
                      </form>
                  </div>
              </article>
              <div class="social">
-                <p>Wanna talk to us?, contact us at sales@dshop.me</p>
+                <p>Wanna talk to us?, contact us at <strong>Sales@dshop.me</strong></p>
                  <ul class="social">
                         <li><div class="fb-like" data-href="https://www.facebook.com/dshopeu?ref=hl" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div></li>
                         <li><a href="https://twitter.com/dshop_usa" class="twitter-follow-button" data-show-count="false">Follow @dshop_usa</a>
@@ -104,13 +68,13 @@
                     <li>2. What is the Purchase frecuency on your customers</li>
                   </ul>
                   <p>And more...</p>
-                  <input type="submit" value="Start free trial" />
+                  <input type="submit" value="Start your 30 days free trial" id="call_action_1" class="call_action_green" />
              </article>
  
              <article class="step2_copy">
                   <h2>Hi!, Let me explain to you what Dshop is</h2>
                   <p>Dshop is an inventory and management software for retail stores, Dshop is here to provide you valuable information that can make your business survive and grow on a competitive market.</p>
-                  <input type="submit" value="Start free trial" />
+                  <input type="submit" value="Start your 30 days free trial" id="call_action_11" class="call_action_green" />
              </article>
 
              <article class="separator">
@@ -120,19 +84,19 @@
              <article class="step3">
                 <h2>Great, but now what are your store's benefits here?</h2>
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen1.png" alt="" />
+                    <img src="http://i.imgur.com/CfupwLA.jpg" alt="Dshop is multi-device available" />
                     <h3>All in your pocket</h3>
-                    <p>Manage your store by using your smartphone, laptop or tablet with our mobile app and website. <a href="#" alt="">More..</a></p>
+                    <p>Manage your store by using your smartphone, laptop or tablet with our mobile app and website. <a href="tour.php" alt="Dshop service in your pocket">More..</a></p>
                 </div>
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen2.png" alt="" />
+                    <img src="http://i.imgur.com/mBurLB9.jpg" alt="No needs for expensives machines" />
                     <h3>No expensive machines</h3>
-                    <p>We offer your a cheap and small invoice printer for your store, or you just can use a regular printer. <a href="#" alt="">More..</a></p>
+                    <p>We offer your a cheap and small invoice printer for your store, or you just can use a regular printer. <a href="tour.php" alt="no expensives machines for dshop">More..</a></p>
                 </div>
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen3.png" alt="" />
+                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen3.png" alt="Scan your products with your cellphone" />
                     <h3>Scan with your phone</h3>
-                    <p>Make your business more professional by using your phone camera you will be able to scan your products. <a href="#" alt="">More..</a></p>
+                    <p>Make your business more professional by using your phone camera you will be able to scan your products. <a href="tour.php" alt="scan your products with your phone">More..</a></p>
                 </div>
              </article>
 
@@ -152,19 +116,19 @@
 
              <article class="step5">
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen1.png" alt="" />
+                    <img src="http://i.imgur.com/rVoQR0h.png" alt="Manage your store with Dshop" />
                     <h3>Manage your store</h3>
-                    <p>Add products, report sales, register customers, get accounting reports, generate invoices and more. <a href="#" alt="" >More..</a></p>
+                    <p>Add products, report sales, register customers, get accounting reports, generate invoices and more. <a href="tour.php" alt="manage your store with dshop" >More..</a></p>
                 </div>
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen1.png" alt="" />
+                    <img src="http://i.imgur.com/8N4clWX.png" alt="Dshop will provide you with business information" />
                     <h3>Business information</h3>
-                    <p>We use your store information to give you valuable advices about how to increase your sales and business. <a href="#" alt="">More..</a></p>
+                    <p>We use your store information to give you valuable advices about how to increase your sales and business. <a href="tour.php" alt="get business information from dshop">More..</a></p>
                 </div>
                 <div class="feature_box">
-                    <img src="https://dl.dropboxusercontent.com/u/90178259/imagen1.png" alt="" />
+                    <img src="http://i.imgur.com/w5iDek6.png" alt="Update on real time your store stock" />
                     <h3>Update on real time</h3>
-                    <p>We sync your store inventory with your ecommerce website inventory, sale just the products you have. <a href="#" alt="">More..</a></p>
+                    <p>We sync your store inventory with your ecommerce website inventory, sale just the products you have. <a href="tour.php" alt="update on real time your store stock">More..</a></p>
                 </div>
              </article>
 
@@ -174,8 +138,8 @@
 
              <article class="step6">
                 <h2>Friend what do you think?</h2>
-                <p>Give us a chance, for sure Dshop is going to make a diference in the way you manage your entire business, if you wanna reed more click here  , if you're ready start now your free trial.</p>
-                <input type="submit" value="Start free trial" />
+                <p>Give us a chance, for sure Dshop is going to make a diference in the way you manage your entire business, if you wanna reed more <a href="#">click here</a>  , if you're ready start now your free trial.</p>
+                <input type="submit" value="Get started 30-Day free trial" class="call_action_blue" id="call_action_2" />
              </article>
 
 
@@ -186,17 +150,7 @@
         </section>
         <!--Section****************************************-->
 
-       <!--footer-->
-        <footer>
-            <div class="links">
-            <a href="#">Pricing</a><a href="#"> Tour</a> <a href="#"> Contact us</a></li>
-            <a href="#"> Blog</a><a href="#"> Company</a>
-            </div>
-            <div class="copy">
-             <p>© 2014 Dshop, LLC.</p>
-            </div>
-        </footer>
-       <!--footer-->
+        <?php include ('site/footer.php') ?> <!--footer--> 
 
     </body>
 </html>
